43ce86a6cf6881f6af3c5d8fb03bec371c9c48ac
Gyungwon Na <nkw601@naver.com>
2025-01-22 00:53:54 +0900

    Feat: 볼 스트라이크 판별 로직 구현

--- src/main/java/baseball/model/BaseballGame.java
+++ src/main/java/baseball/model/BaseballGame.java
@@ -2,9 +2,7 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class BaseballGame {
     private final static int MIN_VALUE = 1;
@@ -30,16 +28,52 @@ public class BaseballGame {
                 .toArray(); // List<Integer> -> int[]
     }
 
-    public static boolean hasDuplicateNums(int[] baseballNumber) {
-        return new HashSet<>(Arrays.stream(baseballNumber).boxed().toList()).size() != baseballNumber.length;
+    public static boolean hasDuplicateNums(int[] inputNumbers) {
+        return new HashSet<>(Arrays.stream(inputNumbers).boxed().toList()).size() != inputNumbers.length;
     }
 
-    public static boolean hasNumsOutOfRange(int[] baseballNumber) {
-        for (int num : baseballNumber) {
+    public static boolean hasNumsOutOfRange(int[] inputNumbers) {
+        for (int num : inputNumbers) {
             if (num < 1 || num > 9) {
                 return false;
             }
         }
         return true;
+    }
+
+    // 비교 -> 볼 count
+    public int countBall(int[] answer, int[] inputNumbers) {
+        int ballCount = 0;
+        List<Integer> answerList = convertToList(answer);
+        List<Integer> inputNumbersList = convertToList(inputNumbers);
+
+        for (int i = 0; i < answer.length; i++) {
+            if (answerList.contains(inputNumbers[i]) && inputNumbers[i] != answer[i]) {
+                ballCount++;
+            }
+        }
+
+        return ballCount;
+    }
+
+    // 비교 -> 스트라이크 count
+    public int countStrike(int[] answer, int[] inputNumbers) {
+        int strikeCount = 0;
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == (inputNumbers[i])) {
+                strikeCount++;
+            }
+        }
+
+        return strikeCount;
+    }
+
+    // int[] -> List<Integer> : 아 진짜 리팩토링 할까...
+    public static List<Integer> convertToList(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : array) {
+            list.add(num);
+        }
+        return list;
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,32 @@ package baseball.view;
 
 public class OutputView {
     private final String startingMessage = "숫자 야구 게임을 시작합니다.";
+    private final String failMessage = "낫싱";
+    private final String successMessage = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void startGame() {
         System.out.println(startingMessage);
     }
+
+    public void roundResult(int ballCount, int strikeCount){
+        if (ballCount == 0 && strikeCount == 0){
+            System.out.println(failMessage);
+            return;
+        }
+
+        if (strikeCount == 3){
+            System.out.println(successMessage);
+            return;
+        }
+
+        if (ballCount != 0){
+            System.out.print(ballCount+"볼 ");
+        }
+
+        if (strikeCount != 0){
+            System.out.println(strikeCount+"스트라이크");
+        }
+    }
+
+    public void endGame() {}
 }
